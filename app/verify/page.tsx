34cc6dd9ed262507544400c65e62463e544f57cd
Debian <lovexl@az.pdi44c3xsq2ujcazx2uyqdarha.lx.internal.cloudapp.net>
2026-03-12 09:37:09 +0000

    Wrap verify page request state in Suspense

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { connection } from "next/server";
+import { Suspense } from "react";
 import { TurnstileVerificationPage } from "@/components/turnstile-verification-page";
 import {
   getTurnstileSiteKey,
@@ -13,7 +14,15 @@ type VerifyPageProps = {
   }>;
 };
 
-export default async function Page({ searchParams }: VerifyPageProps) {
+export default function Page({ searchParams }: VerifyPageProps) {
+  return (
+    <Suspense fallback={<div className="flex h-dvh" />}>
+      <VerifyPageContent searchParams={searchParams} />
+    </Suspense>
+  );
+}
+
+async function VerifyPageContent({ searchParams }: VerifyPageProps) {
   await connection();
 
   const [{ redirect: redirectParam }, cookieStore] = await Promise.all([
