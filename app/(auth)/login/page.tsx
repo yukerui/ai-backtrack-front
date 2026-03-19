2f2bade3d2212a5c0f6db3bd0ba56c99031fe220
Debian <lovexl@az.pdi44c3xsq2ujcazx2uyqdarha.lx.internal.cloudapp.net>
2026-03-19 12:07:04 +0000

    Fix auth page prerender redirects

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,27 @@
+import { connection } from "next/server";
+import { Suspense } from "react";
 import {
 	AUTH_REDIRECT_QUERY_PARAM,
 	normalizeAuthRedirectPath,
 } from "@/lib/auth-redirect";
 import { LoginPageClient } from "./login-page-client";
 
-type LoginPageProps = {
+type LoginPageContentProps = {
 	searchParams: Promise<{
 		[AUTH_REDIRECT_QUERY_PARAM]?: string | string[] | undefined;
 	}>;
 };
 
-export default async function Page({ searchParams }: LoginPageProps) {
+export default function Page({ searchParams }: LoginPageContentProps) {
+	return (
+		<Suspense fallback={<div className="flex h-dvh bg-background" />}>
+			<LoginPageContent searchParams={searchParams} />
+		</Suspense>
+	);
+}
+
+async function LoginPageContent({ searchParams }: LoginPageContentProps) {
+	await connection();
 	const search = await searchParams;
 	const redirectTo = search[AUTH_REDIRECT_QUERY_PARAM];
 
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,16 +1,27 @@
+import { connection } from "next/server";
+import { Suspense } from "react";
 import {
 	AUTH_REDIRECT_QUERY_PARAM,
 	normalizeAuthRedirectPath,
 } from "@/lib/auth-redirect";
 import { RegisterPageClient } from "./register-page-client";
 
-type RegisterPageProps = {
+type RegisterPageContentProps = {
 	searchParams: Promise<{
 		[AUTH_REDIRECT_QUERY_PARAM]?: string | string[] | undefined;
 	}>;
 };
 
-export default async function Page({ searchParams }: RegisterPageProps) {
+export default function Page({ searchParams }: RegisterPageContentProps) {
+	return (
+		<Suspense fallback={<div className="flex h-dvh bg-background" />}>
+			<RegisterPageContent searchParams={searchParams} />
+		</Suspense>
+	);
+}
+
+async function RegisterPageContent({ searchParams }: RegisterPageContentProps) {
+	await connection();
 	const search = await searchParams;
 	const redirectTo = search[AUTH_REDIRECT_QUERY_PARAM];
 
